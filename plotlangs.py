9604baefe0c10bafea6ae9226178faa47ddf98a6
Nick K <nick.karamanian@gmail.com>
2019-06-12 14:15:19 -0700

    slope graphs best fit

--- plotlangs.py
+++ plotlangs.py
@@ -11,6 +11,8 @@ import csv
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+import datetime as dt
+import matplotlib.dates as mdates
 
 for lang in languages:
     filename = './languages/' + lang + '.csv'
@@ -35,13 +37,28 @@ for lang in languages:
     plt.ylabel('posts')
     plt.savefig(outputfile, dpi=100)
 
+    years = [i for i in range(2008,2020)]
+    months = [i for i in range(1,13)]
+    dates = []
+    for year in years:
+        for month in months:
+            if (year == 2008 and month < 8) or (year == 2019 and month > 2):
+                continue
+            dates.append(dt.datetime(year,month,1))
+
+    print(diff)
+    x = mdates.date2num(dates)
+    z = np.polyfit(x,diff,52)
+    f = np.poly1d(z)
+    
     outputfile = './languages/' + lang + '_slope' + '.png'
-    fig, ax = plt.subplots(1,1)
-    ax.plot(x,diff)
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+    fig, ax = plt.subplots()
+    xx = np.linspace(x.min(), x.max(), 100)
+    dd = mdates.num2date(xx)
+    ax.plot(dd,f(xx), '-g')
+    ax.plot(dates,diff,'x',color='b')
     plt.gcf().autofmt_xdate()
     plt.gcf().set_size_inches(25.5, 10.5)
     plt.title(lang + ' slope')
-    plt.xlabel('date')
     plt.ylabel('posts')
     plt.savefig(outputfile, dpi=100)
